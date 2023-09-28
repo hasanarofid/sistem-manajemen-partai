2dd19ef2b9b8264f97e33900a1b676689a798c0d
hasanarofid <hasanarofid@gmail.com>
2023-09-28 20:38:00 +0700

    menu sidebar admin

---
+++ resources/views/partials/sidebar_menu_admin.blade.php
@@ -0,0 +1,268 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+               <li class="nav-item">
+            <a href="{{ url('/')  }}" class="nav-link active">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-map-marker-alt"></i>
+              <p>
+                Data Wilayah
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-sitemap"></i>
+                     <p>TPS</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-sitemap"></i>
+                     <p>Kelurahan / Desa</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-sitemap"></i>
+                     <p>Kecamatan</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-sitemap"></i>
+                     <p>Kabupaten/Kota</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-sitemap"></i>
+                     <p>Provinsi</p>
+                  </a>
+               </li>
+
+            </ul>
+         </li>
+
+         <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-heart"></i>
+              <p>
+                Data Relawan
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-users"></i>
+                     <p>Koor Kab/kota</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-users"></i>
+                     <p>Koor Kec</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-users"></i>
+                     <p>Koor Kel/Desa</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-users"></i>
+                     <p>Koor RW</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-users"></i>
+                     <p>Koor TPS</p>
+                  </a>
+               </li>
+
+            </ul>
+         </li>
+
+         <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-address-card"></i>
+              <p>
+                Data Pemilih
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-list"></i>
+                     <p>Kategori Pemilih</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-file-import"></i>
+                     <p>Import Pemilih</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-user"></i>
+                     <p>Koor Kel/Desa</p>
+                  </a>
+               </li>
+
+             
+
+            </ul>
+         </li>
+
+          
+          <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                Calon Relawan
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                Saksi
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+              <i class="nav-icon fas fa-calendar"></i>
+              <p>
+                Event
+              </p>
+            </a>
+          </li>
+
+            <li class="nav-item">
+               <a href="pages/kanban.html" class="nav-link">
+               <i class="nav-icon fas fa-chart-line"></i>
+               <p>
+                  Analisa Kampanye
+               </p>
+               </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-money-bill"></i>
+              <p>
+                Aktivitas Keuangan
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-plus-circle"></i>
+                     <p>Pengeluaran</p>
+                  </a>
+               </li>
+
+               <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-file-import"></i>
+                     <p>Tambah Pengeluaran</p>
+                  </a>
+               </li>
+
+                <li class="nav-item">
+                  <a href="pages/charts/chartjs.html" class="nav-link">
+                     <i class="fas fa-list"></i>
+                     <p>Kategori Pengeluaran</p>
+                  </a>
+               </li>
+
+             
+
+            </ul>
+         </li>
+
+             <li class="nav-item">
+               <a href="#" class="nav-link">
+               <i class="nav-icon fas fa-book"></i>
+               <p>
+                  Laporan
+                  <i class="right fas fa-angle-left"></i>
+               </p>
+               </a>
+               <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                     <a href="pages/charts/chartjs.html" class="nav-link">
+                        <i class="fas fa-plus-circle"></i>
+                        <p>Laporan Keuangan</p>
+                     </a>
+                  </li>
+
+               
+
+               </ul>
+         </li>
+
+         <li class="nav-item">
+               <a href="#" class="nav-link">
+               <i class="nav-icon fas fa-cog"></i>
+               <p>
+                  Pengaturan
+                  <i class="right fas fa-angle-left"></i>
+               </p>
+               </a>
+               <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                     <a href="pages/charts/chartjs.html" class="nav-link">
+                        <i class="fas fa-users"></i>
+                        <p>Semua Akun</p>
+                     </a>
+                  </li>
+
+               
+
+               </ul>
+         </li>
+
+
+  <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Profile
+              </p>
+            </a>
+          </li>
+
+
+        </ul>
+      </nav>
